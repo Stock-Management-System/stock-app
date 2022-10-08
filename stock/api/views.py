86f14286750b5914f8aaefd9de1667a38414e228
FatihG34 <fthgnydn34@gmail.com>
2022-10-08 12:36:28 +0300

    filters added

--- stock/api/views.py
+++ stock/api/views.py
@@ -1,40 +1,47 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework import permissions
-from stock.api.permissions import ProductManagerPermission
 from stock.models import Category, Brand, Firm, Product, Transaction
 from .serializers import CategorySerializer, FirmSerializer, BrandSerializer, TransactionSerializer, ProductSerializer
-from django.db.models import Sum
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
 
 
 class BrandView(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
     permission_classes = [permissions.DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
 
 
 class FirmView(viewsets.ModelViewSet):
     queryset = Firm.objects.all()
     serializer_class = FirmSerializer
     permission_classes = [permissions.DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
 
 
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ["name"]
+    filterset_fields = ("category", "brand")
 
 
 class TransactionView(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    # permission_classes = [ProductManagerPermission]
-
-    
+    permission_classes = [permissions.DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["firm"]
 
 
